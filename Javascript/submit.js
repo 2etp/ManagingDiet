846c85fa393b83f44722c0d23b0b13d646e5d49c
BlancPong <bu3820@naver.com>
2022-07-25 16:34:01 +0900

    결과값 단계별 페이지 구현 및 css 수정, 스타일 위치 조정

---
+++ Javascript/submit.js
@@ -0,0 +1,24 @@
+(function () {
+    $(document).on('click', '.btn-submit', function () {
+        if (!$(this).hasClass('loading')) {
+            $(this).addClass('loading');
+            let self = this;
+
+            setTimeout(function () {
+                $(self).removeClass('loading');
+                $(self).addClass('checked');
+            }, 1500);
+        }
+    });
+})();
+
+// 2.5초후에 frm라는 id를 가진 form를 submit
+window.onload = function () {
+    document.getElementById('btn').onclick = function () {
+        setTimeout(function () {
+            document.getElementById('frm').submit();
+            location.href = 'choice_diet2.html'
+            return false;
+        }, 2500);
+    };
+};
