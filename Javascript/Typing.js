687ed1819ff77b7bb7ae58567378d95f212edb35
BlancPong <bu3820@naver.com>
2022-07-28 16:19:04 +0900

    step1: 배경 이미지 수정, step2, step3: 타이핑 애니메이션 속도 수정, step4, 제목 폰트 및 전체적인 색상 톤 수정

--- Javascript/Typing.js
+++ Javascript/Typing.js
@@ -4,15 +4,15 @@ var tag = document.getElementById("text");
 var html = document.getElementById("text").innerHTML;
 var attr = tag.setAttribute("data", html);
 var txt = tag.getAttribute("data");
-var speed = 50;
+var speed = 30;
 
 function typeWriter() {
-    if (i <= txt.length) {
-        document.getElementById("text").innerHTML = txt.slice(0, i + 1);
-        i++;
-        setTimeout(typeWriter, speed);
-    }
-    //console.log(document.getElementById("text").innerHTML);
+	if (i <= txt.length) {
+		document.getElementById("text").innerHTML = txt.slice(0, i + 1);
+		i++;
+		setTimeout(typeWriter, speed);
+	}
+	//console.log(document.getElementById("text").innerHTML);
 }
 
 typeWriter();
