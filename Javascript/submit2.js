7ce6b4a48ffe63f66ba3174caf39e34d1496e011
BlancPong <bu3820@naver.com>
2022-07-29 11:58:05 +0900

    step2 javascript로 활동량을 선택 안할시 sumit 못하게 구현

--- Javascript/submit2.js
+++ Javascript/submit2.js
@@ -14,11 +14,25 @@
 
 // 2.5초후에 frm라는 id를 가진 form를 submit
 window.onload = function () {
+    let cho1 = document.getElementById('choice_1')
+    let cho2 = document.getElementById('choice_2')
+    let cho3 = document.getElementById('choice_3')
+    let cho4 = document.getElementById('choice_4')
+    let cho5 = document.getElementById('choice_5')
     document.getElementById('btn').onclick = function () {
         setTimeout(function () {
-            document.getElementById('frm').submit();
-            location.href = 'choice_diet3.html'
-            return false;
+            if (cho1.checked == false &&
+                cho2.checked == false &&
+                cho3.checked == false &&
+                cho4.checked == false &&
+                cho5.checked == false) {
+                alert("활동량을 선택해주세요!")
+            }
+            else {
+                document.getElementById('frm').submit();
+                location.href = 'choice_diet3.html'
+                return false;
+            }
         }, 2500);
     };
 };
