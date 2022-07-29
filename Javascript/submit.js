74258291080d732f9ce200faed54496f00239d15
BlancPong <bu3820@naver.com>
2022-07-29 11:50:16 +0900

    step1 javascript로 성별 지정 안할시 경고 alert 띄움, 신장 100~250 사이로 입력 안할시 alert 띄움, 체중을 1~250 사이로 입력 안할시 alert 띄움, 나이를 1~150 사이로 입력 안할시 alert 띄움,

--- Javascript/submit.js
+++ Javascript/submit.js
@@ -14,11 +14,32 @@
 
 // 2.5초후에 frm라는 id를 가진 form를 submit
 window.onload = function () {
+    let man = document.getElementById('gentle1')
+    let woman = document.getElementById('gentle2')
+    let inp1 = document.getElementById('inp')
+    let inp2 = document.getElementById('inp2')
+    let inp3 = document.getElementById('inp3')
     document.getElementById('btn').onclick = function () {
         setTimeout(function () {
-            document.getElementById('frm').submit();
-            location.href = 'choice_diet2.html'
-            return false;
+            if (man.checked == false && woman.checked == false) {
+                alert("성별을 지정해주세요!")
+            }
+            else if (inp1.value < '100' || inp1.value > '250') {
+                alert("신장을 100~250 사이로 입력하세요!")
+            }
+            else if (inp2.value < '1' || inp2.value > '250') {
+                alert("체중을 1~250 사이로 입력하세요!")
+            }
+            else if (inp3.value < '1' || inp3.value > '150') {
+                alert("나이를 1~150 사이로 입력하세요!")
+            }
+            else {
+                if ((man.checked == true && woman.checked == false) || (man.checked == false && woman.checked == true)) {
+                    document.getElementById('frm').submit();
+                    location.href = 'choice_diet2.html'
+                    return false;
+                }
+            }
         }, 2500);
     };
 };
