8cec48577095010f40015cc19ac6cafae1ad1745
BlancPong <bu3820@naver.com>
2022-08-05 12:59:06 +0900

    로그인 js 수정 및 스탬프 미션 css 및 js 수정중..

--- Javascript/login.js
+++ Javascript/login.js
@@ -102,203 +102,203 @@ jQuery(document).ready(function ($) {
 
     //IE9 placeholder fallback
     //credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
-    if (!Modernizr.input.placeholder) {
-        $('[placeholder]').focus(function () {
-            var input = $(this);
-            if (input.val() == input.attr('placeholder')) {
-                input.val('');
+    //     if (!Modernizr.input.placeholder) {
+    //         $('[placeholder]').focus(function () {
+    //             var input = $(this);
+    //             if (input.val() == input.attr('placeholder')) {
+    //                 input.val('');
+    //             }
+    //         }).blur(function () {
+    //             var input = $(this);
+    //             if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    //                 input.val(input.attr('placeholder'));
+    //             }
+    //         }).blur();
+    //         $('[placeholder]').parents('form').submit(function () {
+    //             $(this).find('[placeholder]').each(function () {
+    //                 var input = $(this);
+    //                 if (input.val() == input.attr('placeholder')) {
+    //                     input.val('');
+    //                 }
+    //             })
+    //         });
+    //     }
+
+    // });
+
+
+    //credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+    jQuery.fn.putCursorAtEnd = function () {
+        return this.each(function () {
+            // If this function exists...
+            if (this.setSelectionRange) {
+                // ... then use it (Doesn't work in IE)
+                // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+                var len = $(this).val().length * 2;
+                this.setSelectionRange(len, len);
+            } else {
+                // ... otherwise replace the contents with itself
+                // (Doesn't work in Google Chrome)
+                $(this).val($(this).val());
             }
-        }).blur(function () {
-            var input = $(this);
-            if (input.val() == '' || input.val() == input.attr('placeholder')) {
-                input.val(input.attr('placeholder'));
-            }
-        }).blur();
-        $('[placeholder]').parents('form').submit(function () {
-            $(this).find('[placeholder]').each(function () {
-                var input = $(this);
-                if (input.val() == input.attr('placeholder')) {
-                    input.val('');
-                }
-            })
         });
-    }
+    };
 
-});
-
-
-//credits https://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
-jQuery.fn.putCursorAtEnd = function () {
-    return this.each(function () {
-        // If this function exists...
-        if (this.setSelectionRange) {
-            // ... then use it (Doesn't work in IE)
-            // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
-            var len = $(this).val().length * 2;
-            this.setSelectionRange(len, len);
-        } else {
-            // ... otherwise replace the contents with itself
-            // (Doesn't work in Google Chrome)
-            $(this).val($(this).val());
-        }
-    });
-};
-
-
-// 비밀번호 확인
-$('.pw').focusout(function () {
-    let pwd1 = $("#password_1").val();
-    let pwd2 = $("#password_2").val();
-    if (pwd1 == '' && pwd2 == '') {
-        null;
-    } else if (pwd1 != "" || pwd2 != "") {
-        if (pwd1 == pwd2) {
-            $("#alert-success").css('display', 'block');
-            $("#alert-danger").css('display', 'none');
-            setTimeout(function () {
-                $("#alert-success").css('display', 'none');
-            }, 3000)
-
-        } else {
-            $("#alert-success").css('display', 'none');
-            $("#alert-danger").css('display', 'block');
-            setTimeout(function () {
-                $("#alert-danger").css('display', 'none');
-            }, 3000)
-        }
-    }
-});
-
-// 회원가입 예외처리
-// 비밀번호 유효성 검사 문법
-function checkPassword() {
-    let userName = $("#signup-username").val();
-    let userEmail = $("#signup-email").val();
-    let userPhone = $("#signup-phone").val();
-    let pwd1 = $("#password_1").val();
-    let pwd2 = $("#password_2").val();
-    let agreechecked = $("#accept-terms");
-
-    // 이름 확인
-    if (userName == '') {
-        $("#user-danger").css('display', 'block')
-        setTimeout(function () {
-            $("#user-danger").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    // 이름 특수문자 사용막는 정규식
-    else if (!/^[가-힝a-zA-Z]{2,}$/.test(userName)) {
-        $("#user-danger2").css('display', 'block')
-        setTimeout(function () {
-            $("#user-danger2").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    // 이메일 확인
-    else if (userEmail == '') {
-        $("#email-danger").css('display', 'block')
-        setTimeout(function () {
-            $("#email-danger").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    // 이메일 정규식
-    else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/.test(userEmail)) {
-        $("#email-danger2").css('display', 'block')
-        setTimeout(function () {
-            $("#email-danger2").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-
-    // 휴대폰 번호 확인
-    else if (userPhone == '') {
-        $("#phone-danger").css('display', 'block')
-        setTimeout(function () {
-            $("#phone-danger").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-
-    else if (!/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/.test(userPhone)) {
-        $("#phone-danger2").css('display', 'block')
-        setTimeout(function () {
-            $("#phone-danger2").css('display', 'none');
-        }, 3000)
-        return false;
-    }
 
     // 비밀번호 확인
-    else if (pwd1 !== pwd2) {
-        $('#password_1').val('').focus();
-        $('#password_2').val('').focus();
-        return false;
-    }
-    // 비밀번호 정규식
-    else if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd1)) {
-        if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd2)) {
-            $("#password-danger").css('display', 'block');
-            $("#password-danger3").css('display', 'none');
-            $('#password_1').val('').focus();
+    $('.pw').focusout(function () {
+        let pwd1 = $("#password_1").val();
+        let pwd2 = $("#password_2").val();
+        if (pwd1 == '' && pwd2 == '') {
+            null;
+        } else if (pwd1 != "" || pwd2 != "") {
+            if (pwd1 == pwd2) {
+                $("#alert-success").css('display', 'block');
+                $("#alert-danger").css('display', 'none');
+                setTimeout(function () {
+                    $("#alert-success").css('display', 'none');
+                }, 3000)
+
+            } else {
+                $("#alert-success").css('display', 'none');
+                $("#alert-danger").css('display', 'block');
+                setTimeout(function () {
+                    $("#alert-danger").css('display', 'none');
+                }, 3000)
+            }
+        }
+    });
+
+    // 회원가입 예외처리
+    // 비밀번호 유효성 검사 문법
+    function checkPassword() {
+        let userName = $("#signup-username").val();
+        let userEmail = $("#signup-email").val();
+        let userPhone = $("#signup-phone").val();
+        let pwd1 = $("#password_1").val();
+        let pwd2 = $("#password_2").val();
+        let agreechecked = $("#accept-terms");
+
+        // 이름 확인
+        if (userName == '') {
+            $("#user-danger").css('display', 'block')
             setTimeout(function () {
-                $("#password-danger").css('display', 'none');
-            }, 5000)
+                $("#user-danger").css('display', 'none');
+            }, 3000)
             return false;
         }
-    }
-    // 약관 동의 체크 안할시
-    else if ($(agreechecked).prop("checked") == false) {
-        $("#accept-danger").css('display', 'block')
-        setTimeout(function () {
-            $("#accept-danger").css('display', 'none');
-        }, 3000)
-        return false;
+        // 이름 특수문자 사용막는 정규식
+        else if (!/^[가-힝a-zA-Z]{2,}$/.test(userName)) {
+            $("#user-danger2").css('display', 'block')
+            setTimeout(function () {
+                $("#user-danger2").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        // 이메일 확인
+        else if (userEmail == '') {
+            $("#email-danger").css('display', 'block')
+            setTimeout(function () {
+                $("#email-danger").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        // 이메일 정규식
+        else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/.test(userEmail)) {
+            $("#email-danger2").css('display', 'block')
+            setTimeout(function () {
+                $("#email-danger2").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+
+        // 휴대폰 번호 확인
+        else if (userPhone == '') {
+            $("#phone-danger").css('display', 'block')
+            setTimeout(function () {
+                $("#phone-danger").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+
+        else if (!/^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/.test(userPhone)) {
+            $("#phone-danger2").css('display', 'block')
+            setTimeout(function () {
+                $("#phone-danger2").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+
+        // 비밀번호 확인
+        else if (pwd1 !== pwd2) {
+            $('#password_1').val('').focus();
+            $('#password_2').val('').focus();
+            return false;
+        }
+        // 비밀번호 정규식
+        else if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd1)) {
+            if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(pwd2)) {
+                $("#password-danger").css('display', 'block');
+                $("#password-danger3").css('display', 'none');
+                $('#password_1').val('').focus();
+                setTimeout(function () {
+                    $("#password-danger").css('display', 'none');
+                }, 5000)
+                return false;
+            }
+        }
+        // 약관 동의 체크 안할시
+        else if ($(agreechecked).prop("checked") == false) {
+            $("#accept-danger").css('display', 'block')
+            setTimeout(function () {
+                $("#accept-danger").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+
+        else {
+            $('#signup-form').submit();
+        }
     }
 
-    else {
-        $('#signup-form').submit();
-    }
-}
 
 
-
-function loginchk() {
-    let loginEmail = $("#signin-email").val();
-    let loginPw = $("#signin-password").val();
-    // 로그인 이메일 입력 안할시
-    if (loginEmail == '') {
-        $("#login-email").css('display', 'block')
-        setTimeout(function () {
-            $("#login-email").css('display', 'none');
-        }, 3000)
-        return false;
+    function loginchk() {
+        let loginEmail = $("#signin-email").val();
+        let loginPw = $("#signin-password").val();
+        // 로그인 이메일 입력 안할시
+        if (loginEmail == '') {
+            $("#login-email").css('display', 'block')
+            setTimeout(function () {
+                $("#login-email").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        // 로그인 이메일 정규식
+        else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/.test(loginEmail)) {
+            $("#login-email2").css('display', 'block')
+            setTimeout(function () {
+                $("#login-email2").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        // 로그인 비밀번호 입력안할시
+        else if (loginPw == '') {
+            $("#login-pw").css('display', 'block')
+            setTimeout(function () {
+                $("#login-pw").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        // 로그인 비밀번호 정규식
+        else if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(loginPw)) {
+            $("#login-pw2").css('display', 'block')
+            setTimeout(function () {
+                $("#login-pw2").css('display', 'none');
+            }, 3000)
+            return false;
+        }
+        else {
+            $('#signin-form').submit();
+        }
     }
-    // 로그인 이메일 정규식
-    else if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/.test(loginEmail)) {
-        $("#login-email2").css('display', 'block')
-        setTimeout(function () {
-            $("#login-email2").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    // 로그인 비밀번호 입력안할시
-    else if (loginPw == '') {
-        $("#login-pw").css('display', 'block')
-        setTimeout(function () {
-            $("#login-pw").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    // 로그인 비밀번호 정규식
-    else if (!/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(loginPw)) {
-        $("#login-pw2").css('display', 'block')
-        setTimeout(function () {
-            $("#login-pw2").css('display', 'none');
-        }, 3000)
-        return false;
-    }
-    else {
-        $('#signin-form').submit();
-    }
-}
